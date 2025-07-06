a69012faf72068780bc267331dabaf7a0de3aba2
fromtheeast710 <theeast710@proton.me>
2025-07-06 14:04:37 +1000

    feat: basic setup

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixvim.url = "github:nix-community/nixvim";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = {
+    nixvim,
+    flake-parts,
+    ...
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
+      perSystem = {system, ...}: let
+        nixvimLib = nixvim.lib.${system};
+        nixvim' = nixvim.legacyPackages.${system};
+        nixvimModule = {
+          inherit system;
+          module = import ./config;
+          extraSpecialArgs = {
+            # inherit (inputs) foo;
+          };
+        };
+        nvim = nixvim'.makeNixvimWithModule nixvimModule;
+      in {
+        checks = {
+          default = nixvimLib.check.mkTestDerivationFromNixvimModule nixvimModule;
+        };
+
+        packages.default = nvim;
+      };
+    };
+}
---
+++ justfile
@@ -0,0 +1,8 @@
+# run nixvim
+r:
+  nix run .
+# check config
+c:
+  nix flake check .
+i:
+  nix profile install .
